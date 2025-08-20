dbbf5b861031176f99888e68ed5bc6d7a534f951
Chris Greening <chris.greening@mitt.ca>
2025-08-20 09:11:21 -0500

    Added support for schema

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pyodbc
+import sys
+import argparse
 
 def get_sql_connection():
     # Read connection string from file
@@ -121,10 +123,80 @@ def script_insert_with_related(table_name, id, conn, visited=None, schema='dbo',
             )
     return script_list
 
+def script_delete_with_related(table_name, id, conn, schema='dbo', visited=None, script_list=None):
+    if visited is None:
+        visited = set()
+    if script_list is None:
+        script_list = []
+    key = (schema, table_name, id)
+    if key in visited:
+        return script_list
+    visited.add(key)
+
+    cursor = conn.cursor()
+    # Get primary key identity column
+    cursor.execute(f"""
+        SELECT COLUMN_NAME
+        FROM INFORMATION_SCHEMA.COLUMNS
+        WHERE TABLE_NAME = ? AND COLUMNPROPERTY(object_id('[{schema}].[{table_name}]'), COLUMN_NAME, 'IsIdentity') = 1
+    """, table_name)
+    cols = cursor.fetchone()
+    if cols is None:
+        raise Exception("Unable to find identity column for table " + table_name)
+    identity_col = cols[0]
+
+    # Find referencing tables and columns
+    cursor.execute(f"""
+        SELECT fk_schema.name AS referencing_schema, fk_tab.name AS referencing_table, fk_col.name AS referencing_column
+        FROM sys.foreign_key_columns fkc
+        INNER JOIN sys.tables pk_tab ON pk_tab.object_id = fkc.referenced_object_id
+        INNER JOIN sys.columns pk_col ON pk_col.column_id = fkc.referenced_column_id AND pk_col.object_id = pk_tab.object_id
+        INNER JOIN sys.tables fk_tab ON fk_tab.object_id = fkc.parent_object_id
+        INNER JOIN sys.columns fk_col ON fk_col.column_id = fkc.parent_column_id AND fk_col.object_id = fk_tab.object_id
+        INNER JOIN sys.schemas fk_schema ON fk_schema.schema_id = fk_tab.schema_id
+        WHERE pk_tab.name = ?
+    """, table_name)
+    referencing = cursor.fetchall()
+
+    for ref_schema, ref_table, ref_column in referencing:
+        # Find rows in referencing table that point to this id
+        cursor.execute(f"""
+            SELECT {ref_column}
+            FROM [{ref_schema}].[{ref_table}]
+            WHERE {ref_column} = ?
+        """, id)
+        rows = cursor.fetchall()
+        for row in rows:
+            ref_id = row[0]
+            # Recursively delete referencing rows
+            script_delete_with_related(
+                ref_table, ref_id, conn, schema=ref_schema,
+                visited=visited, script_list=script_list
+            )
+
+    # Delete the main row
+    delete_sql = f"DELETE FROM [{schema}].[{table_name}] WHERE [{identity_col}] = {id};"
+    script_list.append(delete_sql)
+    return script_list
+
 def main():
+    parser = argparse.ArgumentParser(description='Generate SQL scripts for inserting or deleting rows with related data.')
+    parser.add_argument('action', choices=['insert', 'delete'], help='Action to perform: insert or delete')
+    parser.add_argument('table_name', help='Name of the table to operate on')   
+    parser.add_argument('id', help='ID of the row to operate on')
+    parser.add_argument('--schema', default='dbo', help='Schema of the table (default: dbo)')
+    args = parser.parse_args()
+    action = args.action
+    table_name = args.table_name
+    id = args.id
+    schema = args.schema
+    id = sys.argv[3]
     conn = get_sql_connection()
-    inserts = script_insert_with_related('Applications', 1, conn)
-    print('\n\n'.join(inserts))
+    if action == 'delete':
+        lines = script_delete_with_related(table_name, id, conn, schema=schema)
+    else:
+        lines = script_insert_with_related(table_name, id, conn, schema=schema)
+    print('\n\n'.join(lines))
 
 if __name__ == "__main__":
     main()
